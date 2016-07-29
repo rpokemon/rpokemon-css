628c19f4282acf8a11a1291c4648c1c5dec5485a
Matthew Lee <matthewhlee96@gmail.com>
2016-07-28 18:51:16 -0700

    Revert "variables test"
    This reverts commit 6f89a0a19fc580340b7e4453a2b7b0bc354e764d.

--- compress.py
+++ compress.py
@@ -1,4 +1,4 @@
-import re
+
 import os.path
 import shutil
 import glob
@@ -6,39 +6,11 @@ import time
 import datetime
 from csscompressor import compress
 
-rep = {}
-
-def get_vars():
-    global rep
-    with open("variables.dat", "r") as f:
-        for line in f:
-            lstrip = line.strip()
-            if (len(lstrip) == 0 || lstrip.startswith('#') || lstrip.startswith('//')):
-                continue
-                
-            line_data = line.split("=", 1)
-            rep[("$" + line_data[0].strip() + "$")] = line_data[1].strip()
-
-def rep_vars(text):
-    global rep
-    print(rep)
-    
-    if not rep:
-        return text;
-    
-    rep = dict((re.escape(k), v) for k, v in rep.items())
-    pattern = re.compile("|".join(rep.keys()))
-    text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
-    return text
-            
 def top():
     return (
         '/*CreatedBy:Hero_of_Legend,technophonix1,Atooz,kwwxis*/')
 
 def run():
-    global rep
-    get_vars()
-    
     # Combine files in specified order
     with open('dist.css', 'wb') as outfile:
         with open('poketheme.css', 'rb') as readfile:
@@ -46,7 +18,7 @@ def run():
                 
     # Compress
     with open('dist.css', 'r+') as outfile:
-        raw  = rep_vars(outfile.read()) # replace vars too
+        raw  = outfile.read()
         mini = compress(raw)
         outfile.seek(0)
         outfile.write(top() + '\n' + mini)
