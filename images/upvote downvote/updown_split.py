3350c28ffe05b79dcf9e2897e615b5ae3099308e
Joshua Butt <josh.ja.butt@gmail.com>
2018-10-27 23:40:00 +1000

    add support for converting updown images into individual files

---
+++ images/upvote downvote/updown_split.py
@@ -0,0 +1,8 @@
+from PIL import Image
+
+img = Image.open('downupletsgo.png')
+
+for i, f in enumerate(['up_a', 'up', 'down_a', 'down']):
+    y = img.size[0] * i
+    c = img.crop((0, y, img.size[0], y+img.size[0]))
+    c.save(f'{f}.png')
