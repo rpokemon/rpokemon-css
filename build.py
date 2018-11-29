77a6758db14c59297805e85b1c7c2e169e8006f9
Joshua Butt <josh.ja.butt@gmail.com>
2018-11-30 02:25:15 +1000

    move flairBG config to seperate file

--- build.py
+++ build.py
@@ -38,6 +38,8 @@ order = ['etc.css',            # body, buttons, and wiki
 
          'flair.css',          # user flairs
          'userflair.css',      # user flairs
+         'ballflair.css',       # user flairs
+         'flairbg.css',        # user flairs
          'moontheme.css',      # night mode
          'banner.css',         # banner
          'misc.css'            # miscellaneous
