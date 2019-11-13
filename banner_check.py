05a6a113f277d805c0218a2b96b555930de9642a
Josh B <josh.ja.butt@gmail.com>
2019-11-14 06:37:44 +1000

    Add node package configuration
    - clean up banner check for funsies.

--- banner_check.py
+++ banner_check.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import random
 import string
 import time
@@ -16,57 +17,62 @@ samples = [
     {
         '*': 'k'
     }
-    
-]
 
-bucket = {}
+]
+bucket = Counter()
+
 
 def gen_hash(length):
-    return ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(length))
-    
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+
+
 start_time = time.time()
+
+
 for i in range(C):
-    hash = gen_hash(N);
-    
+    hash = gen_hash(N)
+
     print(hash)
-    
+
     for s in samples:
         name = ''
-            
-        if ('^' not in s and '*' not in s and '$' not in s):
+
+        if all(c not in s for c in ['^', '*', '$']):
             continue
-        
-        if ('^' in s and s['^'] is not None):
-            name += '[uh^='+s['^']+']'
-            if (not hash.startswith(s['^'])):
+
+        if s.get('^') is not None:
+            contain_val = s['^']
+            name += f'[uh^={contain_val}]'
+            if not hash.startswith(contain_val):
                 continue
-        if ('*' in s and s['*'] is not None):
+
+        if s.get('*') is not None:
             contain_val = s['*']
-            if type(contain_val) is list:
-                skip = False
-                for j in contain_val:
-                    name += '[uh*='+j+']'
-                    if (j not in hash):
-                        skip = True
-                        break
-                if skip:
+            if isinstance(contain_val, list):
+                try:
+                    for j in contain_val:
+                        name += f'[uh*={j}]'
+                        if j not in hash:
+                            raise StopIteration()
+                except StopIteration:
                     continue
             else:
-                name += '[uh*='+contain_val+']'
-                if (contain_val not in hash):
+                name += f'[uh*={contain_val}]'
+                if contain_val not in hash:
                     continue
-        if ('$' in s and s['$'] is not None):
-            name += '[uh^='+s['$']+']'
-            if (not hash.startswith(s['$'])):
+
+        if s.get('$') is not None:
+            contain_val = s['$']
+            name += f'[uh$={contain_val}]'
+            if not hash.endswith(contain_val):
                 continue
-        
-        if name not in bucket:
-            bucket[name] = 1
-        else:
-            bucket[name] += 1
+
+        bucket[name] += 1
+
+
 end_time = time.time()
 
-print('\nCompleted in ' + str(end_time - start_time) + ' seconds\n')
+print(f'\nCompleted in {end_time - start_time:.2f} seconds\n')
 
-for key,val in bucket.items():
-    print('  ' + key + ': ' + str(val / C * 100.0) + '% ('+str(val)+')')
+for key, val in bucket.items():
+    print(f'\t{key}: {val/C:.2%} ({val})')
